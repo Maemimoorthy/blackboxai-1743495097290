e8bb1669065cbe37a3d187023751371fc3b1c74b
Maemimoorthy <maemimoorthy28@gmail.com>
2025-04-01 13:42:58 +0530

    Add zomato-clone/backend/config/swagger.js 1743495151531

---
+++ zomato-clone/backend/config/swagger.js
@@ -0,0 +1,35 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Zomato Clone API',
+      version: '1.0.0',
+      description: 'API documentation for Zomato Clone application',
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000/api',
+        description: 'Development server',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        }
+      }
+    },
+    security: [{
+      bearerAuth: []
+    }]
+  },
+  apis: ['./routes/*.js', './models/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = specs;
